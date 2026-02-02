17666db4133b02ba8bd56914e873cba9ce1cbf16
Vedant <shrivastavvedant15@gmail.com>
2026-02-02 23:13:06 +0530

    initial commit

---
+++ landing/src/App.jsx
@@ -0,0 +1,14 @@
+// import "./App.css";
+import Navbar from "./Navbar";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
